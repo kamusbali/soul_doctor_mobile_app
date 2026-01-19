2c32935b2d599ba9625920e67d98655d7790d9c6
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2026-01-19 15:25:36 +0800

    try: try again to fix error on xcode cloud

--- ci_scripts/xcode_cloud_post_clone.sh
+++ ci_scripts/xcode_cloud_post_clone.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-echo "Xcode Cloud post-clone script running..."
+echo "Running Xcode Cloud post-clone script..."
 
-# Setup Flutter
-export PATH="$PATH:/opt/flutter/bin"
+# Download Flutter SDK
+FLUTTER_VERSION="3.19.0"
+curl -o flutter.zip https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_$FLUTTER_VERSION-stable.zip
+unzip -q flutter.zip
+export PATH="$PATH:$(pwd)/flutter/bin"
 
+# Flutter setup
+flutter doctor -v
 flutter pub get
-
 flutter build ios --no-codesign
 
+# Install CocoaPods
 cd ios
 pod install
