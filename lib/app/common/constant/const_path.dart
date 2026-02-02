a9ce4df55d934add5b0d31070d2182584b1d1034
putrimhdw <mahadewicahayaputri@gmail.com>
2026-02-02 09:44:52 +0800

    fix: try to fix the environment and app store connect

--- lib/app/common/constant/const_path.dart
+++ lib/app/common/constant/const_path.dart
@@ -1,25 +1,27 @@
 abstract class ConstPath {
   // path
-  static const DEVELOPMENT_PATH = "conf/development.env";
-  static const PRODUCTION_PATH = "conf/production.env";
-  static const STAGING_PATH = "conf/staging.env";
+  static var DEVELOPMENT_PATH = "conf/development.env";
+  static var PRODUCTION_PATH = "conf/production.env";
+  static var STAGING_PATH = "conf/staging.env";
+
+  static final _imageDirectory = "assets/img";
 
   // Image
-  static const ICON_APP_PATH = "assets/img/icon.png";
-  static const CARD_ILLUSTRATION_CHECK_MENTAL_CONDITION_PATH =
-      "assets/img/card_illustration_check_mental_condition.png";
-  static const CARD_ILLUSTRATION_CONSULTATION_PATH =
-      "assets/img/card_illustration_consultation.png";
-  static const BACKGROUND_HEADER_PATH = "assets/img/background_header.png";
-  static const DIALOG_ILLUSTRATION_UNAUTHORIZE_PATH =
-      "assets/img/dialog_illustration_unauthorize.png";
-  static const BACKGROUND_AUTHENTICATION_PROFILE_PATH =
-      "assets/img/background_authentication_profile.png";
+  static var ICON_APP_PATH = "$_imageDirectory/icon.png";
+  static var CARD_ILLUSTRATION_CHECK_MENTAL_CONDITION_PATH =
+      "$_imageDirectory/card_illustration_check_mental_condition.png";
+  static var CARD_ILLUSTRATION_CONSULTATION_PATH =
+      "$_imageDirectory/card_illustration_consultation.png";
+  static var BACKGROUND_HEADER_PATH = "$_imageDirectory/background_header.png";
+  static var DIALOG_ILLUSTRATION_UNAUTHORIZE_PATH =
+      "$_imageDirectory/dialog_illustration_unauthorize.png";
+  static var BACKGROUND_AUTHENTICATION_PROFILE_PATH =
+      "$_imageDirectory/background_authentication_profile.png";
 
-  static const ILLUSTRATION_RESULT_GOOD_PATH =
-      "assets/img/illustration_result_good.png";
-  static const ILLUSTRATION_RESULT_BAD_PATH =
-      "assets/img/illustration_result_bad.png";
+  static var ILLUSTRATION_RESULT_GOOD_PATH =
+      "$_imageDirectory/illustration_result_good.png";
+  static var ILLUSTRATION_RESULT_BAD_PATH =
+      "$_imageDirectory/illustration_result_bad.png";
 
-      static const AVATAR_DEFAULT_PATH = "assets/img/avatar";
+  static var AVATAR_DEFAULT_PATH = "$_imageDirectory/avatar";
 }
