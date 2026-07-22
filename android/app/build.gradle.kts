65ca972919ce7bd6eaa40432dc9189932af7d53c
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2026-07-22 19:07:52 +0800

    fix: fix the android suffix package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -66,16 +66,12 @@ android {
     productFlavors {
         create("development") {
             dimension = "app"
-            applicationIdSuffix = ".dev"
-            versionNameSuffix = "-dev"
             manifestPlaceholders["app_name"] = "[DEV] Harmoni Jiwa"
             resValue("string", "app_name", "[DEV] Harmoni Jiwa")
         }
 
         create("stagging") {
             dimension = "app"
-            applicationIdSuffix = ".stage"
-            versionNameSuffix = "-stage"
             manifestPlaceholders["app_name"] = "[STAGE] Harmoni Jiwa"
             resValue("string", "app_name", "[STAGE] Harmoni Jiwa")
         }
